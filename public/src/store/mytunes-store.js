14330d1b777945f94e1d297f944f916d20b00eda
Daniel O'Connell <doconnelldesign@gmail.com>
2018-01-25 10:02:48 -0700

    fixed route store

--- public/src/store/mytunes-store.js
+++ public/src/store/mytunes-store.js
@@ -22,7 +22,7 @@ var store = new vuex.Store({
       var url = '//bcw-getter.herokuapp.com/?url=';
       var url2 = 'https://itunes.apple.com/search?term=' + artist;
       var apiUrl = url + encodeURIComponent(url2);
-     $.getJSON('https://vue-music-oconnell.herokuapp.com/music/songs')
+     $.getJSON(url2)
       .then(songs => {
         commit('setMyTunes', songs)
       })
